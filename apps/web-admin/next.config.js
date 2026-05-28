5cc6ed35842b65550500e0898d46470a2d2632ae
akshidhaa <akshidhaabalaji@gmail.com>
2026-05-28 08:18:55 +0530

    fix: resolve shared packages for Netlify build

--- apps/web-admin/next.config.js
+++ apps/web-admin/next.config.js
@@ -1,6 +1,17 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@scr-mesh/types", "@scr-mesh/constants", "@scr-mesh/playbooks"],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@scr-mesh/types': path.resolve(__dirname, '../../shared/types/src/index.ts'),
+      '@scr-mesh/constants': path.resolve(__dirname, '../../shared/constants/src/index.ts'),
+      '@scr-mesh/playbooks': path.resolve(__dirname, '../../shared/playbooks/src/index.ts'),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
